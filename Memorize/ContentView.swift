ae430b0859d2748bed7ca9d5fc3f18bf1d88a432
Irineu Peres <irineugpd@unipam.edu.br>
2021-09-27 22:38:12 -0300

    first commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Irineu Gustavo Peres on 27/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            ForEach(0..<4) { index in
+                CardView()
+            }
+        }
+            .padding()
+            .foregroundColor(Color.purple)
+            .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = true
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
